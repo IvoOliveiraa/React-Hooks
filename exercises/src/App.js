b9923e9a2083d7c6269752eefb1ce578908064f3
Ivo Oliveira <ivo793@gmail.com>
2024-04-17 01:25:32 -0300

    useContext

--- exercises/src/App.js
+++ exercises/src/App.js
@@ -1,62 +1,12 @@
 import './App.css';
-import { useEffect, useMemo, useRef, useState } from 'react';
-import P from 'prop-types';
-
-const Post = ({ post, handleClick }) => {
-  return (
-    <div key={post.id} className="post">
-      <h1 onClick={() => handleClick(post.title)}> {post.title} </h1>
-      <p> {post.body} </p>
-    </div>
-  );
-};
-
-Post.propTypes = {
-  post: P.shape({
-    id: P.number,
-    title: P.string,
-    body: P.string,
-  }),
-  handleClick: P.func,
-};
+import { AppContext } from './contexts/AppContext';
+import { Div } from './components/Div';
 
 function App() {
-  const [post, setPosts] = useState([]);
-  const [value, setValue] = useState('');
-  const input = useRef(null);
-
-  useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then((r) => r.json())
-      .then((r) => setPosts(r));
-  }, []);
-
-  useEffect(() => {
-    input.current.focus();
-    console.log(input.current);
-  }, [value]);
-
-  const handleClick = (value) => {
-    setValue(value);
-  };
-
   return (
-    <div className="App">
-      <p>
-        <input
-          ref={input}
-          type="search"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-        />
-      </p>
-
-      {useMemo(() => {
-        return post.map((post) => {
-          return <Post key={post.id} post={post} handleClick={handleClick} />;
-        });
-      }, [post])}
-    </div>
+    <AppContext>
+      <Div />
+    </AppContext>
   );
 }
 
