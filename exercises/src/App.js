1c3281d601cbef2d53c675d3a6f40e5f7538a686
Ivo Oliveira <ivo793@gmail.com>
2024-04-22 21:42:12 -0300

    useContext+useReducer

--- exercises/src/App.js
+++ exercises/src/App.js
@@ -1,50 +1,67 @@
+import P from 'prop-types';
 import './App.css';
-import { useReducer } from 'react';
+import { createContext, useContext, useReducer, useRef } from 'react';
 
-const globaState = {
+export const actions = {
+  CHANGE_TITLE: 'CHANGE_TITLE',
+};
+
+export const globaState = {
   title: 'O título do contexto',
-  bodu: 'O body do contexto',
+  body: 'O body do contexto',
   counter: 0,
 };
 
-const reducer = (state, action) => {
+export const reducer = (state, action) => {
   switch (action.type) {
-    case 'mudar': {
-      console.log('Chamou mudar com', action.payload);
+    case actions.CHANGE_TITLE: {
+      console.log('Mudar título');
       return { ...state, title: action.payload };
     }
-    case 'inverter': {
-      console.log('Chamou inverter');
-      const { title } = state;
-      return { ...state, title: title.split('').reverse().join('') };
-    }
   }
+
   return { ...state };
 };
 
-function App() {
+export const Context = createContext();
+export const AppContext = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, globaState);
-  const { counter, title, body } = state;
+
+  const changeTitle = (payLoad) => {
+    dispatch({ type: actions.CHANGE_TITLE, payLoad });
+  };
 
   return (
-    <div>
-      <h1>
-        {title}
-        {counter}
-      </h1>
+    <Context.Provider value={{ state, changeTitle }}>
+      {children}
+    </Context.Provider>
+  );
+};
 
-      <button
-        onClick={() =>
-          dispatch({
-            type: 'mudar',
-            payload: new Date().toLocaleString('pt-BR'),
-          })
-        }
-      >
-        Click
-      </button>
-      <button onClick={() => dispatch({ type: 'inverter' })}>Invert</button>
-    </div>
+AppContext.propTypes = {
+  children: P.node,
+};
+
+export const H1 = () => {
+  const context = useContext(Context);
+  const inputRef = useRef();
+  return (
+    <>
+      <h1 onClick={() => context.changeTitle(inputRef.current.value)}>
+        {context.state.title}{' '}
+      </h1>
+      <input type="text" ref={inputRef} />
+    </>
+  );
+};
+
+function App() {
+  return (
+    <AppContext>
+      <div>
+        <H1 />
+      </div>
+    </AppContext>
   );
 }
 
