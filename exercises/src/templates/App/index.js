325796c1c8eba5759112c43e0bac30fb50577d02
Ivo Oliveira <ivo793@gmail.com>
2024-04-29 03:32:28 -0300

    creating new hook

--- exercises/src/templates/App/index.js
+++ exercises/src/templates/App/index.js
@@ -1,109 +1,13 @@
-import { useEffect, useRef, useState } from 'react';
-
-const isObjectEqual = (objA, objB) => {
-  return JSON.stringify(objA) === JSON.stringify(objB);
-};
-
-const useFetch = (url, options) => {
-  const [resulte, setResulte] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [shouldLoad, setShouldLoad] = useState(false);
-  const urlRef = useRef(url);
-  const optionsRef = useRef(options);
-
-  useEffect(() => {
-    let changed = false;
-
-    if (!isObjectEqual(url, urlRef.current)) {
-      urlRef.current = url;
-      changed = true;
-    }
-    if (!isObjectEqual(options, optionsRef.current)) {
-      optionsRef.current = options;
-      changed = true;
-    }
-
-    if (changed) {
-      setShouldLoad((s) => !s);
-    }
-  }, [url, options]);
-
-  useEffect(() => {
-    let wait = false;
-    const controller = new AbortController();
-    const signal = controller.signal;
-
-    setLoading(true);
-
-    const fetchData = async () => {
-      await new Promise((r) => setTimeout(r, 1000));
-
-      try {
-        const response = await fetch(urlRef.current, {
-          signal,
-          ...optionsRef.current,
-        });
-        const jsonResult = await response.json();
-
-        if (!wait) {
-          setResulte(jsonResult);
-          setLoading(false);
-        }
-      } catch (e) {
-        if (!wait) {
-          setLoading(false);
-        }
-        console.log('MY ERROR', e.message);
-      }
-    };
-
-    fetchData();
-
-    return () => {
-      wait = true;
-      controller.abort();
-    };
-  }, [shouldLoad]);
-
-  return [resulte, loading];
-};
+import { useEffect, useState } from 'react';
 
 export const Home = () => {
-  const [postId, setPostId] = useState('');
-  const [result, loading] = useFetch(
-    'https://jsonplaceholder.typicode.com/posts/' + postId,
-    {
-      headers: {
-        abc: '1' + postId,
-      },
-    },
-  );
+  const [posts, setPosts] = useState(null);
 
-  if (loading) {
-    return <p>Loading...</p>;
-  }
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetch('https://jsonplaceholder.typicode.com/posts');
+    };
+  }, []);
 
-  const handleClick = (id) => {
-    setPostId(id);
-  };
-
-  if (!loading && result) {
-    return (
-      <div>
-        {result?.length > 0 ? (
-          result.map((p) => (
-            <div key={`post-${p.id}`} onClick={() => handleClick(p.id)}>
-              <p>{p.title} </p>
-            </div>
-          ))
-        ) : (
-          <div onClick={() => handleClick('')}>
-            <p>{result.title} </p>
-          </div>
-        )}
-      </div>
-    );
-  }
-
-  return <h1>Teste</h1>;
+  return <h1>{JSON.stringify(posts)} </h1>;
 };
