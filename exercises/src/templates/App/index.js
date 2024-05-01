a49e49de5bb2974e3ad8b4fe1b07752c0d1fa82c
Ivo Oliveira <ivo793@gmail.com>
2024-04-30 21:02:12 -0300

    Compound Components and React.Children + React.cloneElement

--- exercises/src/templates/App/index.js
+++ exercises/src/templates/App/index.js
@@ -1,33 +1,65 @@
-import { useDebugValue, useEffect, useState } from 'react';
+import {
+  Children,
+  cloneElement,
+  createContext,
+  useContext,
+  useState,
+} from 'react';
 
-const useMediaQuery = (queryValue, initialValue = false) => {
-  const [match, setMatch] = useState(initialValue);
-
-  useDebugValue(`Query: ${queryValue}`);
-
-  useEffect(() => {
-    let isMounted = true;
-    const matchMedia = window.matchMedia(queryValue);
-
-    const handleChange = () => {
-      if (!isMounted) return;
-      setMatch(Boolean(matchMedia.matches));
-    };
-
-    matchMedia.addEventListener('change', handleChange);
-    setMatch(!!matchMedia.matches);
-
-    return () => {
-      isMounted = false;
-      matchMedia.removeEventListener('change', handleChange);
-    };
-  }, [queryValue]);
-
-  return match;
+const s = {
+  style: {
+    fontSize: '60px',
+  },
 };
 
+const TurnOnOffContext = createContext();
+
+const TurnOnOff = ({ children }) => {
+  const [isOn, setIsOn] = useState(false);
+  const onTurn = () => setIsOn((s) => !s);
+
+  return (
+    <TurnOnOffContext.Provider value={{ isOn, onTurn }}>
+      {children}
+    </TurnOnOffContext.Provider>
+  );
+};
+
+const TurnedOn = ({ children }) => {
+  const { isOn } = useContext(TurnOnOffContext);
+  return isOn ? children : null;
+};
+const TurnedOff = ({ children }) => {
+  const { isOn } = useContext(TurnOnOffContext);
+  return isOn ? null : children;
+};
+const TurnButton = ({ ...props }) => {
+  const { isOn, onTurn } = useContext(TurnOnOffContext);
+  return (
+    <button onClick={onTurn} {...props}>
+      Turn {isOn ? 'OFF' : 'ON'}{' '}
+    </button>
+  );
+};
+
+const P = ({ children }) => <p {...s}> {children} </p>;
+
 export const Home = () => {
-  const huge = useMediaQuery('(min-width: 980px)');
-  const background = huge ? 'green' : null;
-  return <div style={{ fontSize: '60px', background }}>Teste</div>;
+  return (
+    <TurnOnOff>
+      <div>
+        <header>
+          <TurnedOff>
+            <P>OFF</P>
+          </TurnedOff>
+        </header>
+        <section>
+          <TurnedOn>
+            <P>ON</P>
+          </TurnedOn>
+        </section>
+        <TurnButton {...s} />
+      </div>
+    </TurnOnOff>
+  );
 };
