8ba3b4f75cbbd2584d21189c7146d75a6c38932c
Ivo Oliveira <ivo793@gmail.com>
2024-05-06 21:32:50 -0300

    ContextApi and hooks

--- exercises/src/templates/App/index.js
+++ exercises/src/templates/App/index.js
@@ -1,22 +1,7 @@
-import React, { Suspense, useState } from 'react';
-//import LazyComponent from './lazy-component';
-
-const loadComponent = () => import('./lazy-component');
-const LazyComponent = React.lazy(loadComponent);
-
 export const Home = () => {
-  const [show, setShow] = useState(false);
-
   return (
     <div>
-      <p>
-        <button onMouseOver={loadComponent} onClick={() => setShow((s) => !s)}>
-          Show
-        </button>
-      </p>
-      <Suspense fallback={<p>Carregando...</p>}>
-        {show && <LazyComponent />}
-      </Suspense>
+      <h1>Teste</h1>
     </div>
   );
 };
